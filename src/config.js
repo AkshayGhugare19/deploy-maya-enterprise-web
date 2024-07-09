49312bea892191b725a8b8f0ccddd07db8a5285d
AkshayGhugare19 <114797466+AkshayGhugare19@users.noreply.github.com>
2024-07-09 12:44:31 +0530

    deploy link uploaded

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:3001';
-// export const API_URL = 'https://maya-enterprise-api.onrender.com';
+// export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://deploy-maya-enterprise-api.onrender.com';
 export const ENVIRONMENT = "development"
 export const instId = "1476888";
 export const currency = "GBP";
